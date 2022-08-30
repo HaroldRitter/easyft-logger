6a65215c8beef098a95493b8ac5653a025ef222e
Harold Ritter <harold.ritter@gmail.com>
2022-08-30 21:46:58 +0200

    [Management] Adds few line to the test

--- test.js
+++ test.js
@@ -8,3 +8,9 @@ const logger = new EasyFTLogger();
 
 // Logs something simple
 logger.log("%{bold}Today%{} is a %{italic; orange}sunny%{} day");
+
+// Uses the code syntax
+logger.log("%{1;32}1;32%{}");
+
+// Uses the rgb syntax
+logger.log("%{rgb(50, 212, 126)}rgb(50, 212, 126)%{}");
