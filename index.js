68f39ba5e2e760e7eb5283b21e41a503b47445ef
Harold Ritter <harold.ritter@gmail.com>
2022-08-30 21:37:24 +0200

    Adds the special characters extension

--- index.js
+++ index.js
@@ -4,6 +4,10 @@
 
 const EasyFTLogger = require("./source/EasyFTLogger.class.js");
 
+// ----- Extensions ----- //
+
+require("./source/extensions/special-characters.ext.js");
+
 // ----- Exports EasyFTLogger ----- //
 
 module.exports = EasyFTLogger;
---
+++ source/extensions/special-characters.ext.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/*
+
+	Defines all the default special characters of EasyFTLogger.
+
+*/
+
+const EasyFTLogger = require("../EasyFTLogger.class.js");
+
+Object.assign(EasyFTLogger.specials,
+{
+	"%{":	"%{"
+});
