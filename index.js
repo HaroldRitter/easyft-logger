03efccb074414b9308bcc5bada1a14dbae4ebca1
Harold Ritter <harold.ritter@gmail.com>
2022-08-30 21:35:38 +0200

    Exports EasyFTLogger in the index

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 "use strict";
 
+// ----- Gets the namespace ----- //
+
+const EasyFTLogger = require("./source/EasyFTLogger.class.js");
+
+// ----- Exports EasyFTLogger ----- //
+
+module.exports = EasyFTLogger;
