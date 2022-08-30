e98f64ee8f147445723bb6d6c3d0f7a42f9a9ad6
Harold Ritter <harold.ritter@gmail.com>
2022-08-30 21:40:09 +0200

    Adds the converters extension

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ require("./source/extensions/special-characters.ext.js");
 require("./source/extensions/text-styles.ext.js");
 require("./source/extensions/defined-colors.ext.js");
 
+// All converters are imported in this file
+require("./source/extensions/converter.ext.js");
+
 // ----- Exports EasyFTLogger ----- //
 
 module.exports = EasyFTLogger;
---
+++ source/extensions/converter.ext.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const EasyFTLogger = require("../EasyFTLogger.class.js");
+const Converter = require("../Converter.class.js");
+
+EasyFTLogger.Converter = Converter;
