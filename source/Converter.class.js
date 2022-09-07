f859f01532a81facb9a4588408d8ccdbfcb54fea
Harold Ritter <harold.ritter@gmail.com>
2022-09-07 20:51:56 +0200

    [ES6] Uses the class syntax for Converter

--- source/Converter.class.js
+++ source/Converter.class.js
@@ -1,30 +1,34 @@
 "use strict";
 
-function Converter(name, regExp, convert)
+class Converter
 {
-	// name: String
-	//	The converter's name.
-	this.name = name;
+// ------> Converter - Constructor
 
-	// regExp: RegExp
-	//	The regular expression used to checks if
-	//	a code must be treated with this Converter.
-	//	All results for each code are passed to the
-	//	convert method
-	this.regExp = regExp;
+	constructor(name, regExp, convert)
+	{
+		// name: String
+		//	The converter's name.
+		this.name = name;
 
-	// convert: Function(result[, ... args]): String
-	//	Converts the code found in an EasyFTLogger statement.
-	this.convert = convert;
-};
+		// regExp: RegExp
+		//	The regular expression used to checks if
+		//	a code must be treated with this Converter.
+		//	All results for each code are passed to the
+		//	convert method
+		this.regExp = regExp;
 
-Object.assign(Converter.prototype,
-{
-	toString: function()
+		// convert: Function(result[, ... args]): String
+		//	Converts the code found in an EasyFTLogger statement.
+		this.convert = convert;
+	}
+
+// ------> Converter - PUBLIC Methods
+
+	toString()
 	{
 		return 	"[EasyFTLogger.Converter] " + this.name +
 				" (" + this.regExp.source + ")";
 	}
-});
+}
 
 module.exports = Converter;
